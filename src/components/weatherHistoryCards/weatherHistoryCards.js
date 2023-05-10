adecbafe0d5c3ef8837ef02e6c3d2d0b829ff135
Vova <vova@brodsay.com>
2023-05-10 15:26:44 +0300

    fix: date format

--- src/components/weatherHistoryCards/weatherHistoryCards.js
+++ src/components/weatherHistoryCards/weatherHistoryCards.js
@@ -1,26 +1,23 @@
-import React from "react";
-import Icon from "../../Icon/index";
-import { getDateFromDT, sliceTemp } from "../../utils";
+import React from 'react';
+import Icon from '../../Icon/index';
+import { getDateFromDT, sliceTemp } from '../../utils';
 
 const WeatherHistoryCards = (props) => {
-
   const { weather } = props;
   const weatherForecast = weather?.list;
 
   return (
-    <div className='weekHistoryCards'>
-      {!!weather?.list && weatherForecast.map((key, index) => (
-        <div key={key.dt.toString()} className='historyCard'>
-          <h4>{getDateFromDT(weatherForecast[index].dt, "MM")}</h4>
-          <Icon
-            size='small'
-            weatherType={weatherForecast[index].weather[0].main.toLowerCase()}
-          />
-          <h5>{sliceTemp(weatherForecast[index].temp.day)}°</h5>
-        </div>
-      ))}
+    <div className="weekHistoryCards">
+      {!!weather?.list &&
+        weatherForecast.map((key, index) => (
+          <div key={key.dt.toString()} className="historyCard">
+            <h4>{getDateFromDT(weatherForecast[index].dt, 'DD MMM')}</h4>
+            <Icon size="small" weatherType={weatherForecast[index].weather[0].main.toLowerCase()} />
+            <h5>{sliceTemp(weatherForecast[index].temp.day)}°</h5>
+          </div>
+        ))}
     </div>
   );
-}
+};
 
 export default WeatherHistoryCards;
